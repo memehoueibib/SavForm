5c23c1e84f2660b48537891bcab4b00efec2097a
Meme Houeibib <38425746+memehoueibib@users.noreply.github.com>
2024-02-26 18:25:15 +0100

    Creation du fichier principal

---
+++ savform.php
@@ -0,0 +1,41 @@
+<?php
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class SavForm extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'savform';
+        $this->tab = 'front_office_features';
+        $this->version = '1.0.0';
+        $this->author = 'Meme HOUEIBIB';
+        $this->need_instance = 0;
+        $this->ps_versions_compliancy = ['min' => '1.6', 'max' => _PS_VERSION_];
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('SAV Form');
+        $this->description = $this->l('Fournit un formulaire SAV pour les clients.');
+
+        $this->confirmUninstall = $this->l('Êtes-vous sur de vouloir désinstaller ?');
+    }
+
+    public function install()
+    {
+        if (!parent::install() || !$this->registerHook('displayCustomerAccount')) {
+            return false;
+        }
+        return true;
+    }
+
+    public function uninstall()
+    {
+        if (!parent::uninstall()) {
+            return false;
+        }
+        return true;
+    }
+}
